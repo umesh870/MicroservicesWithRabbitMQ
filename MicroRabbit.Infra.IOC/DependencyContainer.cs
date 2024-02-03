b8a15b123128b3f61586cceaa7db5ebcf1dbea17
umesh870 <us880000@gmail.com>
2024-02-03 19:09:48 +0530

    Initial Commit for Infrastructure Dependency Project

---
+++ MicroRabbit.Infra.IOC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace MicroRabbit.Infra.IOC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
